9a246abbbb679002ccb5311a317a1aab1f0ffd6c
A Silv <silvalexis26@gmail.com>
2024-09-05 17:39:43 -0600

    solution for swaglabs load
    Signed-off-by: A Silv <silvalexis26@gmail.com>

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    testIsolation: false
+    testIsolation: false,
   },
+  chromeWebSecurity: false
 });
